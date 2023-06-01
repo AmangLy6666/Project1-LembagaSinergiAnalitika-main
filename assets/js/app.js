1917b7a2cc27c1d0c047d465f34851f5fd4f20aa
AmangLy666 <tpamangly@gmail.com>
2023-06-02 03:11:17 +0800

    add to do list

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,3 +10,38 @@ window.onscroll = function() {
 AOS.init({
     duration: 800,
 });
+
+// Switch Toggle
+const toggleSwitch = document.querySelector('.toggle-switch');
+const checkbox = toggleSwitch.querySelector('.checkbox');
+const body = document.body;
+
+function toggleMode(isDarkMode) {
+  if (isDarkMode) {
+    body.classList.add('dark-mode');
+    checkbox.checked = true;
+  } else {
+    body.classList.remove('dark-mode');
+    checkbox.checked = false;
+  }
+}
+
+toggleSwitch.addEventListener('click', function() {
+  if (checkbox.checked) {
+    toggleMode(false);
+    localStorage.setItem('isDarkMode', 'false');
+  } else {
+    toggleMode(true);
+    localStorage.setItem('isDarkMode', 'true');
+  }
+});
+
+const storedMode = localStorage.getItem('isDarkMode');
+
+if (storedMode === 'true') {
+  toggleMode(true);
+} else {
+  toggleMode(false);
+}
+
+
