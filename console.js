cecd38d3c681241abf23a7dc0d8285c67d5adb68
Abu Ammar <104537377+amaralkaff@users.noreply.github.com>
2023-06-30 07:04:14 +0800

    Update console.js

--- console.js
+++ console.js
@@ -1,3 +1,16 @@
 // hey whatssup
 console.log("Yooo")
 console.log("Supp")
+
+function simpleArraySum(ar) {
+  // Initialize a variable to store the sum
+  let sum = 0;
+
+  // Iterate through the array and add each element to the sum
+  for (let i = 0; i < ar.length; i++) {
+    sum += ar[i];
+  }
+
+  // Return the sum
+  return sum;
+}
